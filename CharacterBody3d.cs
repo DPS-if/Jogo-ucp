a268281e9e0e126f07a83cda88e0bd07ba1a71c3
caiof-dot <caio.fernandes@estudante.iftm.edu.br>
2026-04-13 09:05:28 -0300

    AddMoveScriptPlayer
    movimentação do player

---
+++ CharacterBody3d.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class CharacterBody3d : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
